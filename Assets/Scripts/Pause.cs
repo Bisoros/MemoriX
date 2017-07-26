2a99e1dfd753de1a4ea22cf257b6afb7a339cc79
BisOros <orinsion2000@gmail.com>
2017-07-26 18:43:20 +0300

    Update Pause.cs

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,5 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +5,7 @@ public class Pause : MonoBehaviour
 {
     public GameObject overlay, menuButton;
 
+    //pause buttons
 	public void resume()
     {
         Time.timeScale = 1;
